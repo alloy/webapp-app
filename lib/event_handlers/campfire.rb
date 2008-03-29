340e79d6049833b6d8f733377081b7677c663a8e
Eloy Duran <eloy.de.enige@gmail.com>
2008-03-29 12:37:57 +0100

    Fixed the way that the username is parsed form the document. Which could have been incorrect in the previous implementation.

--- lib/event_handlers/campfire.rb
+++ lib/event_handlers/campfire.rb
@@ -29,12 +29,10 @@ module Campfire
     # Get the username.
     on_page_loaded do |url, title|
       if @first_name.nil?
-        if row = document.find('#chat tr').last
-          user_id = row['class'].to_s.scan(/user_\d+/).first
-          if username = document.find(:first, "##{user_id} span")
-            @first_name, @last_name = username.textContent.to_s.split(' ')
-            log.debug "Parsed username: #{@first_name} #{@last_name}"
-          end
+        user_id = document.find('script').last.innerHTML.to_s.match(/"userID": (\d+)/)[1]
+        if username = document.find(:first, "#user_#{user_id} span")
+          @first_name, @last_name = username.textContent.to_s.split(' ')
+          log.debug "Parsed username: #{@first_name} #{@last_name}"
         end
       end
     end
