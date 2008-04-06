9c74d6453e38e62c18c77c75dbab650285563056
Eloy Duran <eloy.de.enige@gmail.com>
2008-04-06 17:19:27 +0200

    Campfire: Hide the Lobby link in the main tab bar.

--- lib/event_handlers/campfire.rb
+++ lib/event_handlers/campfire.rb
@@ -4,6 +4,11 @@ module Campfire
   class All < WebApp::EventHandler
     # Hide the room tabs at the top of the page
     css %{
+      #MainTabs li:first-child a
+      {
+        display: none;
+      }
+      
       #MainTabs a.chat
       {
         display: none;
