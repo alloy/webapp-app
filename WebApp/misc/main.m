d6e77ce5d2faca9187b3e2d6007dce0735d7ca57
Eloy Duran <eloy.de.enige@gmail.com>
2008-04-12 16:08:45 +0200

    Added initial WebApp skeleton.

---
+++ WebApp/misc/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  WebApp
+//
+//  Created by YOUR NAME on 2008-04-12.
+//  Copyright (c) 2007 YOUR NAME. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
