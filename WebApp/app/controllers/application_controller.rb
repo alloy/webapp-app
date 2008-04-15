cee661e423764f5d5b31bbc8a4ef34bf0f5d1d16
Eloy Duran <eloy.de.enige@gmail.com>
2008-04-15 22:07:12 +0200

    Add the found bundles to the preset popup menu and set defaults when one is chosen.

--- WebApp/app/controllers/application_controller.rb
+++ WebApp/app/controllers/application_controller.rb
@@ -1,20 +1,36 @@
 class ApplicationController < Rucola::RCController
   ib_outlet :main_window
+  ib_outlet :bundles_menu
+  ib_outlet :name_text_field
+  ib_outlet :url_text_field
   
   def awakeFromNib
+    @bundles_menu.addItemsWithTitles(bundles)
+    @bundles_menu.itemArray.each do |item|
+      item.target = self
+      item.action = 'presetChosen:'
+    end
+  end
+  
+  def presetChosen(item)
+    @name_text_field.stringValue, @url_text_field.stringValue = (if item.title == 'None'
+      ['', '']
+    else
+      preset = defaults[item.title]
+      [preset['name'], preset['url']]
+    end)
   end
   
   private
   
   def bundles
-    Dir.glob("#{Rucola::RCApp.root_path}/bundles/*.wabundle").map { |bundle| File.basename(bundle) }
+    Dir.glob("#{Rucola::RCApp.root_path}/bundles/*.wabundle").map { |bundle| bundle.scan(/\/(\w+)\.wabundle$/).first.first }
   end
   
-  def bundle_menu_items
-    bundles.map do |name|
-      item = OSX::NSMenuItem.alloc.init
-      item.title = name
-      item
-    end
+  def defaults
+    {
+      'Campfire' => { 'name' => 'Campfire', 'url' => 'http://CHANGEME.campfirenow.com' },
+      'Twitter' => { 'name' => 'Twitter', 'url' => 'http://twitter.com/CHANGEME' }
+    }.to_ns
   end
 end
--- WebApp/test/controllers/test_application_controller.rb
+++ WebApp/test/controllers/test_application_controller.rb
@@ -4,19 +4,55 @@ describe 'ApplicationController, in general' do
   tests ApplicationController
   
   def after_setup
+    ib_outlets :bundles_menu => OSX::NSPopUpButton.alloc.init,
+               :name_text_field => OSX::NSTextField.alloc.init,
+               :url_text_field => OSX::NSTextField.alloc.init
+               
     @bundles = ['Foo.wabundle', 'Bar.wabundle']
+    @bundle_names = @bundles.map { |name| name.sub(/\.wabundle$/, '') }
   end
   
   it "should return an array of existing bundles" do
     Dir.stubs(:glob).with("#{Rucola::RCApp.root_path}/bundles/*.wabundle").returns(@bundles.map { |name| "/some/path/to/webapp/#{name}" })
-    controller.send(:bundles).should == @bundles
+    controller.send(:bundles).should == @bundle_names
   end
   
   it "should return an array of menu items for existing bundles" do
-    controller.stubs(:bundles).returns(@bundles)
-    controller.send(:bundle_menu_items).each_with_index do |menu_item, idx|
-      menu_item.should.be.instance_of OSX::NSMenuItem
-      menu_item.title.should == @bundles[idx]
+    controller.stubs(:bundles).returns(@bundle_names)
+    controller.send(:awakeFromNib)
+    bundles_menu.itemArray.map { |item| item.title.to_s }.should == @bundle_names
+    bundles_menu.itemArray.each do |item|
+      item.target.should.be controller
+      item.action.should == 'presetChosen:'
     end
+  end
+  
+  it "should set defaults if a preset was chosen" do
+    defaults = {
+      'Foo' => { 'name' => 'Foo', 'url' => 'http://foo.example.com' },
+      'Bar' => { 'name' => 'Bar', 'url' => 'http://bar.example.com' }
+    }.to_ns
+    
+    controller.stubs(:bundles).returns(@bundle_names)
+    controller.stubs(:defaults).returns(defaults)
+    
+    item = OSX::NSMenuItem.alloc.init
+    item.title = 'Bar'
+    controller.presetChosen(item)
+    
+    name_text_field.stringValue.should == 'Bar'
+    url_text_field.stringValue.should == 'http://bar.example.com'
+  end
+  
+  it "should empty the form elements if the 'None' preset is chosen" do
+    name_text_field.stringValue = 'Foo'
+    url_text_field.stringValue = 'http://foo.example.com'
+    
+    item = OSX::NSMenuItem.alloc.init
+    item.title = 'None'
+    controller.presetChosen(item)
+    
+    name_text_field.stringValue.should.be.empty
+    url_text_field.stringValue.should.be.empty
   end
 end
